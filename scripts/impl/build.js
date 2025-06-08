2272b873df1b90e9c2d7a7278d7dba4d352e5a52
Jakub Elias <wurducius@gmail.com>
2025-06-08 17:39:09 +0200

    Inject view on compile

--- scripts/impl/build.js
+++ scripts/impl/build.js
@@ -14,27 +14,53 @@ const CWD = process.cwd()
 const publicPath = path.join(CWD, "public")
 const distPath = path.join(CWD, "dist")
 
+const MARKER_STYLE_TAG_END = "</head>"
+
 const copyPublicDir = (source, target) => {
   fs.promises.readdir(source, { recursive: true }).then((dir) =>
     dir.map((file) => {
       const sourcePath = path.join(source, file)
       const targetPath = path.join(target, file)
+
+      if (file === "index.html") {
+        return fs.promises.readFile(sourcePath).then((buffer) => {
+          const content = buffer.toString()
+          const split = content.split(MARKER_STYLE_TAG_END)
+          const headOld = split[0]
+
+          const description = "All inclusive web framework with zero configuration, batteries included!"
+          const stylesBase = fs.readFileSync(path.join(CWD, "resources", "base.css")).toString()
+          const stylesView = fs.readFileSync(path.join(CWD, "project", "index.css")).toString()
+          const styles = stylesBase + " " + stylesView
+
+          const headNext = headOld + `<meta name="description" content="${description}"><style>${styles}</style>`
+          const injected = split.map((part, i) => (i === 0 ? headNext : part)).join(MARKER_STYLE_TAG_END)
+          return fs.promises.writeFile(targetPath, injected)
+        })
+      } else {
+        return fs.promises.cp(sourcePath, targetPath)
+      }
+
       return fs.promises.cp(sourcePath, targetPath)
     }),
   )
 }
 
-const buildWebpack = () => {
-  webpack(webpackConfig, (err, stats) => {
+const buildWebpack = (callback) => {
+  return webpack(webpackConfig, (err, stats) => {
     if (err || stats.hasErrors()) {
       console.log(`Webpack error: ${err}`)
     } else {
       console.log(`Project built at ${distPath}`)
     }
+    if (callback) {
+      callback()
+    }
   })
 }
 
 export const build = () => {
-  buildWebpack()
-  copyPublicDir(publicPath, distPath)
+  buildWebpack(() => {
+    copyPublicDir(publicPath, distPath)
+  })
 }
--- webpack/eofol-compile.js
+++ webpack/eofol-compile.js
@@ -1,5 +1,63 @@
-// eslint-disable-next-line no-unused-vars
+import path from "path"
+import fs from "node:fs"
+
 export const eofolCompile = async (compiler, compilation) => {}
 
+const getAsset = ({ asset, nextSource, nextSize, nextInfo }) => {
+  const map = asset ? asset.map() : null
+  return {
+    source: () => nextSource,
+    map: () => map,
+    sourceAndMap: () => ({
+      source: nextSource,
+      map,
+    }),
+    size: () => nextSize,
+    info: nextInfo,
+  }
+}
+
+const CWD = process.cwd()
+
 // eslint-disable-next-line no-unused-vars
-export const processViews = async (compiler, compilation) => {}
+export const processViews = async (compiler, compilation) => {
+  const sourcePath = path.join(CWD, "public", "index.html")
+  const MARKER_STYLE_TAG_END = "</head>"
+
+  fs.promises.readFile(sourcePath).then((buffer) => {
+    const content = buffer.toString()
+    const split = content.split(MARKER_STYLE_TAG_END)
+    const headOld = split[0]
+
+    const description = "All inclusive web framework with zero configuration, batteries included!"
+    const stylesBase = fs.readFileSync(path.join(CWD, "resources", "base.css")).toString()
+    const stylesView = fs.readFileSync(path.join(CWD, "project", "index.css")).toString()
+    const styles = stylesBase + " " + stylesView
+
+    const headNext = headOld + `<description>${description}</description><style>${styles}</style>`
+    return split.map((part, i) => (i === 0 ? headNext : part)).join(MARKER_STYLE_TAG_END)
+  })
+
+  const assetName = "index.html"
+  const nextSource = (
+    await fs.promises.readFile(sourcePath).then((buffer) => {
+      const content = buffer.toString()
+      const split = content.split(MARKER_STYLE_TAG_END)
+      const headOld = split[0]
+
+      const description = "All inclusive web framework with zero configuration, batteries included!"
+      const stylesBase = fs.readFileSync(path.join(CWD, "resources", "base.css")).toString()
+      const stylesView = fs.readFileSync(path.join(CWD, "project", "index.css")).toString()
+      const styles = stylesBase + " " + stylesView
+
+      const headNext = headOld + `<meta name="description" content="${description}"><style>${styles}</style>`
+      return split.map((part, i) => (i === 0 ? headNext : part)).join(MARKER_STYLE_TAG_END)
+    })
+  ).toString()
+
+  compilation.assets[assetName] = getAsset({
+    nextSource,
+    nextInfo: { optimized: false },
+    nextSize: nextSource.length,
+  })
+}
