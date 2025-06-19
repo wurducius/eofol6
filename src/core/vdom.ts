3f660b55f3436d98016a5b963c2caec68a6dca48
Jakub Elias <wurducius@gmail.com>
2025-06-19 02:03:11 +0200

    Fix effect

--- src/core/vdom.ts
+++ src/core/vdom.ts
@@ -1,8 +1,7 @@
-import { VDOMItem } from "../types"
 import { getDef } from "./internal"
-import { logEofolError } from "../eofol-util"
 import { arrayCombinator, mapCombinator } from "../util"
 import { getArgs, Lifecycle } from "./lifecycle"
+import { VDOMItem } from "../types"
 
 const renderTagDom = (vdom: VDOMItem) => {
   const element = document.createElement(vdom.tag)
@@ -20,68 +19,6 @@ const renderTagDom = (vdom: VDOMItem) => {
     })
   }
   return element
-}
-
-const renderCustomDom = (vdom: VDOMItem) => {
-  const def = getDef(vdom.tag)
-  if (def) {
-    const args = getArgs({ vdom, def })
-    const lifecycleArgs = { def, args }
-    if (Lifecycle.shouldRender(lifecycleArgs)) {
-      // @TODO vdom with custom components VS vdom only 1-1 with dom elements
-      const nextVdom = Lifecycle.render(lifecycleArgs)
-      return renderVdomToDom(nextVdom)
-    } else {
-      return vdom
-    }
-  } else {
-    logEofolError(`Def "${vdom.tag}" not found.`)
-  }
-}
-
-const renderDom = (vdom: VDOMItem) => {
-  if (vdom === undefined) {
-    return ""
-  } else if (typeof vdom === "string") {
-    return vdom
-  }
-  if (vdom.type === "custom") {
-    return renderCustomDom(vdom)
-  } else {
-    return renderTagDom(vdom)
-  }
-}
-
-const mountDom = (result, renderedChild) => {
-  if (typeof renderedChild === "string") {
-    result.innerHTML = renderedChild
-  } else if (renderedChild !== undefined) {
-    result.appendChild(renderedChild)
-  }
-}
-
-export const updateDomChildren = (result, vdom: VDOMItem) => {
-  arrayCombinator(vdom.children, (child) => {
-    if (child) {
-      const renderedChild = renderVdomToDom(child)
-      // @TODO process instead as a tree in two steps prevdom -> vdom -> dom
-      mountDom(result, renderedChild)
-    }
-    if (typeof child !== "string" && child && child?.type === "custom") {
-      const def = getDef(child.tag)
-      if (def) {
-        const args = getArgs({ vdom: child, def })
-        Lifecycle.afterRender({ def, args })
-      }
-    }
-  })
-  return result
-}
-
-export const renderVdomToDom = (vdom: VDOMItem) => {
-  const result = renderDom(vdom)
-  updateDomChildren(result, vdom)
-  return result
 }
 
 const renderPrevdomToVdom = (prevdom) => {
@@ -103,9 +40,13 @@ export const traversePreVdom = (prevdom) => {
   } else if (typeof prevdom === "string") {
     return prevdom
   } else {
-    // const key = prevdom.key
-    //  const instance = getInstance(key)
     const rendered = prevdom.render()
+    const def = getDef(rendered.tag)
+    if (def) {
+      // @TODO place after update dom
+      const args = getArgs({ vdom: rendered, def })
+      Lifecycle.afterRender({ def, args })
+    }
     const visited = renderPrevdomToVdom(rendered)
     if (visited !== undefined && typeof visited !== "string" && rendered?.type !== "custom") {
       visited.children = mapCombinator(rendered.children, (child) => {
