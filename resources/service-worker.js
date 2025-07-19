659f7d44836da5b0c51622d2bc2160b8770ba77f
wurducius <wurducius@gmail.com>
2025-07-19 23:56:30 +0200

    Clear warnings

--- resources/service-worker.js
+++ resources/service-worker.js
@@ -21,39 +21,39 @@ self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(cacheId).then((cache) => Promise.all(urlsToCache.map((urlToAdd) => cache.add(urlToAdd)))))
 })
 
-self.addEventListener("activate", (event) => {
-  /**
-   * This event is only called the first time that it is registered in the browser.
-   *
-   * Typically we carry out cache management here - updating/deleting old caches.
-   * We would not want to delete old caches on the install event: See below for full explanation, but it comes down to the fact that the install event is fired
-   * whenever a change to the service_worker.js is detected, BUT the activate event won't kick in until the page is closed down in the browser and re-opened.
-   * So we have a time where the old service worker 'fetch' event is still serving old content - only when the activate event is fired should we delete that content,
-   * as at that point we know that the new service worker is active.
-   *
-   * https://developers.google.com/web/fundamentals/primers/service-workers/#update-a-service-worker
-   */
-})
+//self.addEventListener("activate", (event) => {})
 
-self.addEventListener("fetch", (event) => {
-  /**
-   * This event is NOT called on first installation/activation
-   * But IS called on all subsequent fetch events that are for resources WITHIN the scope of this service_worker.js
-   */
-  /**
-   * During installation, we added the cat.svg resource to a cache.
-   * The name of that cache is not needed for us to retrieve, we can do caches.match('/cat.svg') which will search all caches
-   * with a key of '/cat.svg'.
-   *
-   * In this example below, if we make a request for .com/dog.svg, whilst the request still shows in the network (because the browser has indeed been made it),
-   * it is intercepted here and we provide a response, instead of doing a trip to the server.
-   *
-   * In this example, the response is an entirely different resource altogether!
-   */
-  /*
+/**
+ * This event is only called the first time that it is registered in the browser.
+ *
+ * Typically we carry out cache management here - updating/deleting old caches.
+ * We would not want to delete old caches on the install event: See below for full explanation, but it comes down to the fact that the install event is fired
+ * whenever a change to the service_worker.js is detected, BUT the activate event won't kick in until the page is closed down in the browser and re-opened.
+ * So we have a time where the old service worker 'fetch' event is still serving old content - only when the activate event is fired should we delete that content,
+ * as at that point we know that the new service worker is active.
+ *
+ * https://developers.google.com/web/fundamentals/primers/service-workers/#update-a-service-worker
+ */
+
+//self.addEventListener("fetch", (event) => {})
+
+/**
+ * This event is NOT called on first installation/activation
+ * But IS called on all subsequent fetch events that are for resources WITHIN the scope of this service_worker.js
+ */
+/**
+ * During installation, we added the cat.svg resource to a cache.
+ * The name of that cache is not needed for us to retrieve, we can do caches.match('/cat.svg') which will search all caches
+ * with a key of '/cat.svg'.
+ *
+ * In this example below, if we make a request for .com/dog.svg, whilst the request still shows in the network (because the browser has indeed been made it),
+ * it is intercepted here and we provide a response, instead of doing a trip to the server.
+ *
+ * In this example, the response is an entirely different resource altogether!
+ */
+/*
   const url = new URL(event.request.url)
   if (url.origin === location.origin && url.pathname.endsWith("/dog.svg")) {
     event.respondWith(caches.match("/cat.svg"))
   }
     */
-})
--- src/util/func.ts
+++ src/util/func.ts
@@ -1,11 +1,14 @@
-// eslint-disable-next-line no-unused-vars
-export function arrayCombinator<T>(items: T | T[] | undefined, handler: (_next: T) => void) {
+export function arrayCombinator<T>(
+  items: T | T[] | undefined,
+  // eslint-disable-next-line no-unused-vars
+  handler: (_next: T, _index: number | undefined) => void,
+) {
   if (Array.isArray(items)) {
-    items.forEach((item) => {
-      handler(item)
+    items.forEach((item, index) => {
+      handler(item, index)
     })
   } else if (items) {
-    handler(items)
+    handler(items, undefined)
   }
 }
 
